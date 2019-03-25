3d77cb8622fbfe356419ae21e854f395a2128ec6
Taiwo <taiwo.ibidapoobe@gmail.com>
2019-03-25 15:25:48 +0100

    Displaying the questions and checking for right answers

--- function.js
+++ function.js
@@ -4,11 +4,21 @@ function Question(question, answers, correctAnswer) {
     this.correctAnswer = correctAnswer;
 }
 
-
+// Writing a Prototype to display the questions
 Question.prototype.displayQuestion = function() {
     console.log(this.question);
     for (var i = 0; i < this.answers.length; i++) {
         console.log(i + ': ' + this.answers[i]);
+    }
+}
+
+// Writing a Prototype to check the correct answer with what the user entered in.
+Question.prototype.checkAnswer = function(ans) {
+    if (ans === this.correctAnswer) {
+        console.log('Correct answer!');
+
+    } else {
+        console.log('Wrong answer. Try again :)')
     }
 }
 
@@ -25,5 +35,10 @@ var questions = [q1, q2, q3];
 // Using Math.floor to remove all the decimals
 var num = Math.floor(Math.random() * questions.length);
 
-questions[num];
+questions[num].displayQuestion();
+
+// Using the prompt to display the field to type in the correct answer. Also uning the ParseInt to convert the string to a number
+var answer = parseInt(prompt('Please select the correct answer.'));
+
+questions[num].checkAnswer(answer);
 
