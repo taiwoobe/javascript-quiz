c5de3ef9cd6b303400ef92522e15a27701535ed5
Taiwo <taiwo.ibidapoobe@gmail.com>
2019-03-25 14:52:57 +0100

    First Question Setup

--- function.js
+++ function.js
@@ -1,1 +1,7 @@
-function Question()
+function Question(question, answers, correctAnswer) {
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+}
+
+var q1 = new Question ('Is JavaScript the coolest programming language?', ['Yes', 'No'], 0);
