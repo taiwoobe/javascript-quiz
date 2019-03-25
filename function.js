fae862ea40062dc7d28da3d468d180db80af2b77
Taiwo <taiwo.ibidapoobe@gmail.com>
2019-03-25 14:57:17 +0100

    Additional Questions Added

--- function.js
+++ function.js
@@ -5,3 +5,7 @@ function Question(question, answers, correctAnswer) {
 }
 
 var q1 = new Question ('Is JavaScript the coolest programming language?', ['Yes', 'No'], 0);
+
+var q2 = new Question ('What is the name of the person who invented JavaScript?', ['Mark Goldberg', 'Tom Cruise', 'None of the Above'], 2);
+
+var q3 = new Question ('What best describes coding?', ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
