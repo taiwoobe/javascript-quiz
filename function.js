52e0e455dc13a6154b1fb262f2846e104d24321a
Taiwo <taiwo.ibidapoobe@gmail.com>
2019-03-25 15:11:47 +0100

    Writing a Prototype method to display questions

--- function.js
+++ function.js
@@ -4,8 +4,26 @@ function Question(question, answers, correctAnswer) {
     this.correctAnswer = correctAnswer;
 }
 
+
+Question.prototype.displayQuestion = function() {
+    console.log(this.question);
+    for (var i = 0; i < this.answers.length; i++) {
+        console.log(i + ': ' + this.answers[i]);
+    }
+}
+
 var q1 = new Question ('Is JavaScript the coolest programming language?', ['Yes', 'No'], 0);
 
 var q2 = new Question ('What is the name of the person who invented JavaScript?', ['Mark Goldberg', 'Tom Cruise', 'None of the Above'], 2);
 
 var q3 = new Question ('What best describes coding?', ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
+
+
+var questions = [q1, q2, q3]; 
+
+// Generate random numbers between 0 and the total number of questions.
+// Using Math.floor to remove all the decimals
+var num = Math.floor(Math.random() * questions.length);
+
+questions[num];
+
